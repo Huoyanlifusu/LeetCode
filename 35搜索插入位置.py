2d0f09448c120f41d8ca696b5a7625b1c1b0a345
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-20 17:56:26 +0800

    Create 35搜索插入位置.py

---
+++ 35搜索插入位置.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left,right = 0,len(nums)-1
+        while left<=right:
+            mid = (left+right)//2
+            if target>nums[mid]:
+                left = mid + 1
+            elif target<nums[mid]:
+                right = mid - 1
+            else:
+                return mid
+        return max(left,right)
