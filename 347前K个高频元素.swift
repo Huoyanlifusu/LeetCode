fe49f50146508376d10e02ec07e4c6675ff9296c
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-27 20:10:39 +0800

    Create 347前K个高频元素.swift

---
+++ 347前K个高频元素.swift
@@ -0,0 +1,9 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var map = [Int:Int]()
+        for num in nums {
+            map[num, default: 0] += 1
+        }
+        return Array(map.keys.sorted { map[$0]! > map[$1]! }[0..<k])
+    }
+}
