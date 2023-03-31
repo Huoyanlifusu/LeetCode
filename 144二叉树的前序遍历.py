a06576adce547b10425586f3bae589368784070f
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-31 19:22:47 +0800

    Update 144二叉树的前序遍历.py

--- 144二叉树的前序遍历.py
+++ 144二叉树的前序遍历.py
@@ -1,4 +1,4 @@
-//递归
+#递归
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         nums = []
@@ -9,4 +9,18 @@ class Solution:
             digui(cur.left, nums)
             digui(cur.right, nums)
         digui(root, nums)
+        return nums
+#迭代
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        nums = []
+        stack.append(root)
+        while stack:
+            node = stack[-1]
+            stack.pop()
+            if node: nums.append(node.val)
+            else: continue
+            stack.append(node.right)
+            stack.append(node.left)
         return nums
