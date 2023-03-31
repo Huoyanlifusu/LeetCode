eb7b4995d5119eb47fc7dc61cb7d0116e8f7e0fa
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-03-31 20:21:21 +0800

    Update 145二叉树的后序遍历.py

--- 145二叉树的后序遍历.py
+++ 145二叉树的后序遍历.py
@@ -1,3 +1,4 @@
+#递归
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         nums = []
@@ -10,3 +11,20 @@ class Solution:
         self.digui(cur.left, nums)
         self.digui(cur.right, nums)
         nums.append(cur.val)
+#迭代
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        nums = []
+        stack = []
+        stack.append(root)
+        while stack:
+            node = stack[-1]
+            stack.pop()
+            if node:
+                nums.append(node.val)
+            else:
+                continue
+            stack.append(node.left)
+            stack.append(node.right)
+        nums.reverse()
+        return nums
