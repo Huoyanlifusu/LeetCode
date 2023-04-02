550514001a518d2020d1702f8ced2a00c7f89139
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-03 00:41:15 +0800

    Update 222完全二叉树的节点个数.py

--- 222完全二叉树的节点个数.py
+++ 222完全二叉树的节点个数.py
@@ -15,3 +15,23 @@ class Solution:
                 backsearch(cur.right, stack)
             return len(stack)
         return backsearch(root, stack)
+#判断满树
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        def getNum(node):
+            if not node: return 0
+            left = node.left
+            right = node.right
+            lNum, rNum = 0, 0
+            while left:
+                lNum += 1
+                left = left.left
+            while right:
+                rNum += 1
+                right = right.right
+            if lNum == rNum: return pow(2, lNum+1) - 1
+            lNum = getNum(node.left)
+            rNum = getNum(node.right)
+            result = lNum + rNum + 1
+            return result
+        return getNum(root)
