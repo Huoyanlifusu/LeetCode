cf5ae327bd79b0ab60921a5782b63ece687407e8
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-07 03:34:08 +0800

    Update 455分发饼干.py

--- 455分发饼干.py
+++ 455分发饼干.py
@@ -1,4 +1,5 @@
 #贪心
+#先饼干在胃口
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
@@ -12,4 +13,16 @@ class Solution:
                     index += 1
                     break
                 index += 1
+        return result
+#先胃口再饼干
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        result = 0
+        index = len(s) - 1
+        for i in range(len(g)-1, -1, -1):
+            if index >= 0 and s[index] >= g[i]:
+                index -= 1
+                result += 1
         return result
