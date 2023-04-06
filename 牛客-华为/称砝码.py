4e03dd87c91c0c8ddd0273adab95fe83c9c37349
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-06 15:46:44 +0800

    Update 称砝码.py

--- 牛客-华为/称砝码.py
+++ 牛客-华为/称砝码.py
@@ -1,4 +1,4 @@
-#回溯算法 内存超限制
+#回溯算法 16/20用例 算法复杂度过大
 import sys
 
 categoty = int(input())
@@ -7,17 +7,17 @@ def backsearch(lst, startindex, weight, ans):
     for i in range(startindex, len(lst)):
         if i > 0 and lst[i] == lst[i-1] and i != startindex: continue
         weight += lst[i]
-        ans.append(weight)
+        if weight not in ans: ans.append(weight)
         backsearch(lst, i + 1, weight, ans)
         weight -= lst[i]
     return ans
 
-weights = []
-for line in sys.stdin:
-    w, n = map(int, line.split(" "))
-    while n > 0:
-        weights.append(w)
-
-
-ans = backsearch(weights, 0, 0, [])
-print(ans)
+weights = input().split(" ")
+nums = input().split(" ")
+lst = []
+for index, weight in enumerate(weights):
+    while int(nums[index]) > 0:
+        nums[index] = str(chr(ord(nums[index]) - 1))
+        lst.append(int(weight))
+ans = backsearch(lst, 0, 0, [0])
+print(len(ans))
