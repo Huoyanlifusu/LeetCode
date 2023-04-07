800c3584c6ee6326d807f568f2d503656f399398
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-07 15:13:30 +0800

    Update 491递增子序列.py

--- 491递增子序列.py
+++ 491递增子序列.py
@@ -1,23 +1,19 @@
-#43/56用例
+#树枝剪枝 数层去重 回溯查找组合
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        use = [0 for _ in range(len(nums))]
         arr = []
         result = []
-        def backsearch(nums, startIndex, used):
+        def backsearch(nums, startIndex):
+            if len(arr) > 1: result.append(list(arr))
             if startIndex == len(nums): return
+            uset = set()
             for i in range(startIndex, len(nums)):
-                if (i > 0 and nums[i-1] == nums[i] and used[i-1] == 0) or (arr and nums[i] < arr[-1]):
-                    continue
-                if (i > 1 and nums[i-2] == nums[i-1] == nums[i]) or (i < len(nums) - 2 and nums[i] == nums[i+1] == nums[i+2]):
+                if (nums[i] in uset) or (len(arr) > 0 and nums[i] < arr[-1]):
                     continue
                 arr.append(nums[i])
-                if len(arr) > 1:
-                    result.append(list(arr))
-                used[i] = 1
-                backsearch(nums, i+1, used)
-                used[i] = 0
+                uset.add(nums[i])
+                backsearch(nums, i+1)
                 arr.pop()
 
-        backsearch(nums, 0, use)
+        backsearch(nums, 0)
         return result
