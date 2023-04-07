f4bf0cd44ca85bb7a91551c771002da5908e1e37
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-08 02:21:41 +0800

    Create 377组合总和IV.py

---
+++ 377组合总和IV.py
@@ -0,0 +1,10 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0 for _ in range(target+1)]
+        dp[0] = 1
+        
+        for j in range(1, target+1):
+            for i in range(len(nums)):
+                if j >= nums[i]:
+                    dp[j] += dp[j-nums[i]]
+        return dp[-1]
