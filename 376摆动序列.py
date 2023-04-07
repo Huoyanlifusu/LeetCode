530fc3e2300ea1761ded9b879c3f1af258d5c216
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-07 20:15:25 +0800

    Create 376摆动序列.py

---
+++ 376摆动序列.py
@@ -0,0 +1,13 @@
+#贪心算法
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 1
+        prediff = 0
+        curdiff = 0
+        count = 1
+        for i in range(len(nums) - 1):
+            curdiff = nums[i+1] - nums[i]
+            if (prediff <= 0 and curdiff > 0) or (prediff >= 0 and curdiff < 0):
+                count += 1
+                prediff = curdiff
+        return count
