b5d02279f7cfa8c3979fe49ef2adba6d025a0fb0
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-08 02:35:07 +0800

    Update 70爬楼梯.py

--- 70爬楼梯.py
+++ 70爬楼梯.py
@@ -1,3 +1,4 @@
+#数学归纳法
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [1, 2]
@@ -5,4 +6,17 @@ class Solution:
             return dp[n-1]
         for i in range(2, n):
             dp.append(dp[i-1] + dp[i-2])
+        return dp[-1]
+
+#动态规划
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        nums = [1, 2]
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 1
+        for j in range(1, n+1):
+            for i in range(len(nums)):
+                if j >= nums[i]:
+                    dp[j] += dp[j-nums[i]]
+        
         return dp[-1]
