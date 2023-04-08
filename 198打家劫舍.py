9ab686f4b70dbb6ecefdd7ace7282edd86a6b883
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-08 20:06:11 +0800

    Update 198打家劫舍.py

--- 198打家劫舍.py
+++ 198打家劫舍.py
@@ -1,4 +1,4 @@
-#回溯超时 48/70
+#回溯剪枝
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
@@ -7,6 +7,8 @@ class Solution:
             if i < 0: return 0
             if cache[i] != -1: return cache[i]
             res = max(backsearch(nums, i-1, cache), backsearch(nums, i-2, cache)+nums[i])
+            cache[i] = res
             return res
         
         return backsearch(nums, len(nums)-1, cache)
+#动态规划
