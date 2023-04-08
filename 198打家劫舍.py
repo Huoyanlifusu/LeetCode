da34322a7ae38b5a1d918116f788abf3b0b70c6c
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-08 20:22:45 +0800

    Update 198打家劫舍.py

--- 198打家劫舍.py
+++ 198打家劫舍.py
@@ -12,3 +12,12 @@ class Solution:
         
         return backsearch(nums, len(nums)-1, cache)
 #动态规划
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n+2)
+
+        for i in range(2, n+2):
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i-2])
+        
+        return dp[-1]
