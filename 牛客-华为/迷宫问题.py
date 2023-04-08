a6b3bd31fdfc816bd733903b3701cefaa32076c4
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-09 02:53:20 +0800

    Update 迷宫问题.py

--- 牛客-华为/迷宫问题.py
+++ 牛客-华为/迷宫问题.py
@@ -28,4 +28,28 @@ res = backsearch(m, n, nums, (0,0), (m-1, n-1), [(0,0)], ())
 for step in res:
     print("("+str(step[0])+","+str(step[1])+")")
 
-#bfs 也可行
+#bfs 也可行 python 记录父节点时内存爆表
+import sys
+m, n = map(int, input().split(" "))
+nums = []
+for line in sys.stdin:
+    nums.append(list(map(int, line.split(" "))))
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+end = [m-1, n-1]
+queue = [[0,0]]
+path = []
+while queue:
+    start = queue.pop(0)
+    for i in range(4):
+        newstart = [start[0]+dx[i], start[1]+dy[i]]
+        if newstart[0] >= m or newstart[0] < 0 or newstart[1] >= n or newstart[1] < 0: continue
+        if nums[newstart[0]][newstart[1]] == 1: continue
+        nums[newstart[0]][newstart[1]] = 1
+        queue.append(newstart)
+
+    if start == end:
+        path.append(start)
+
+print(path)
