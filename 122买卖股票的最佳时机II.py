338c854d3ea4aeafbd23cfa58268e24a6f8b60dc
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-10 02:20:20 +0800

    Update 122买卖股票的最佳时机II.py

--- 122买卖股票的最佳时机II.py
+++ 122买卖股票的最佳时机II.py
@@ -1,3 +1,4 @@
+#动态规划
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp = [-prices[0], 0]
@@ -5,3 +6,10 @@ class Solution:
             dp[0] = max(dp[0], dp[1]-prices[i])
             dp[1] = max(dp[1], dp[0]+prices[i])
         return dp[1]
+#贪心算法
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(1, len(prices)):
+            result += max(prices[i]-prices[i-1], 0)
+        return result
