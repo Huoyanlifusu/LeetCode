64dbda0dcd634844186368cfb4d8d357b6c811a0
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-10 18:06:35 +0800

    Update 53最大子数组和.py

--- 53最大子数组和.py
+++ 53最大子数组和.py
@@ -9,3 +9,13 @@ class Solution:
             if count > result: result = count
             if count < 0: count = 0
         return result
+
+#动态规划
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0 for _ in range(n+1)]
+        for i in range(1, n+1):
+            dp[i] = max(dp[i-1]+nums[i-1], nums[i-1])
+        dp.pop(0)
+        return max(dp)
