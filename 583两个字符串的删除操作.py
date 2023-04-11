5fd18249adc16694d21571229cb8c6de31e03801
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-11 17:07:56 +0800

    Create 583两个字符串的删除操作.py

---
+++ 583两个字符串的删除操作.py
@@ -0,0 +1,38 @@
+#动态规划1 列举所有状态
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        for i in range(m+1):
+            dp[i][0] = i
+        for j in range(n+1):
+            dp[0][j] = j
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+2)
+        
+        return dp[m][n]
+#动态规划2 巧用最长公共子序列
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        l = dp[m][n]
+        return m + n - 2*l
