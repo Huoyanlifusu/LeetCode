a897bea9c336298b8b0be78912ac08d23702e55c
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-13 02:40:56 +0800

    Update 矩阵乘法计算量估算.py

--- 牛客-华为/矩阵乘法计算量估算.py
+++ 牛客-华为/矩阵乘法计算量估算.py
@@ -1,4 +1,3 @@
-# 19/20
 import sys
 n = int(input())
 mats = []
@@ -15,7 +14,6 @@ def compute(mat1, mat2):
     return [newrow, newcol, mat1[0]*mat2[1]*mat1[1]]
 
 result = 0
-ref = []
 stack = []
 for char in rule:
     if char == "(":
@@ -23,25 +21,17 @@ for char in rule:
     elif char != ")":
         stack.append(char)
     else:
-        if not ref:
-            mat2 = mats[ord(stack.pop()) - ord('A')]
-            mat1 = mats[ord(stack.pop()) - ord('A')]
-            tmp = compute(mat1, mat2)
-            ref = tmp[0:2]
-            result += tmp[2]
-            stack.append("ref")
-            continue
         k = 2
         tmp = []
         while k > 0:
             k -= 1
             s = stack.pop()
             if s == "ref":
-                tmp.append(ref)
+                tmp.append(mats.pop())
             else:
                 tmp.append(mats[ord(s) - ord('A')])
         res = compute(tmp[1], tmp[0])
-        ref = res[0:2]
+        mats.append(res[0:2])
         result += res[2]
         stack.append("ref")
 
