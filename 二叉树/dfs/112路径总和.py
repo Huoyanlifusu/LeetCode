f8c2a80909208a0382f12fbaa30cedc0597b66ab
Huoyanlifusu <54940667+Huoyanlifusu@users.noreply.github.com>
2023-04-17 20:44:30 +0800

    Update 112路径总和.py

--- 二叉树/dfs/112路径总和.py
+++ 二叉树/dfs/112路径总和.py
@@ -3,3 +3,16 @@ class Solution:
         if not root:
             return targetSum == 0
         return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+#回溯算法
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def backsearch(cur, path):
+            if not cur:
+                return sum(path) == targetSum
+            path.append(cur.val)
+            res1 = backsearch(cur.left, path)
+            res2 = backsearch(cur.right, path)
+            path.pop()
+            return res1 or res2
+        
+        return backsearch(root, [])
